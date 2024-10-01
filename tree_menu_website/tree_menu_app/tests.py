b17843e19cc28051a7319ea08c07bc8f4ccc240b
Nabi <nabi.usonov@gmail.com>
2024-10-01 14:53:00 +0500

    Added test to see how many queries made

--- tree_menu_website/tree_menu_app/tests.py
+++ tree_menu_website/tree_menu_app/tests.py
@@ -1,3 +1,11 @@
-from django.test import TestCase
+from django.test import RequestFactory, TestCase
+from django.template import Template, Context
+from django.urls import reverse
+
 
 # Create your tests here.
+class QueriesMadeTest(TestCase):
+
+    def test_queries_made(self):
+        with self.assertNumQueries(1):
+            self.client.get('/')
